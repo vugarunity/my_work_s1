b985e7f36d53eed0feac813d0a1d7fa95d84ff2b
Your Name <you@example.com>
2024-10-19 16:01:39 +0400

    HotDrinkWithTemperature

---
+++ HotDrinkWithTemperature.java
@@ -0,0 +1,17 @@
+class HotDrinkWithTemperature extends HotDrink {
+    private int temperature;
+
+    public HotDrinkWithTemperature(String name, int volume, int temperature) {
+        super(name, volume);
+        this.temperature = temperature;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", temperature: " + temperature + "°C";
+    }
+}
