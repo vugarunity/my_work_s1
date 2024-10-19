be38dec35d9c449b1e2a51f4966c3648b40019a8
Your Name <you@example.com>
2024-10-19 15:59:56 +0400

    HotDrink

---
+++ HotDrink.java
@@ -0,0 +1,22 @@
+class HotDrink {
+    private String name;
+    private int volume;
+
+    public HotDrink(String name, int volume) {
+        this.name = name;
+        this.volume = volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    @Override
+    public String toString() {
+        return "Drink: " + name + ", объем: " + volume + " мл";
+    }
+}
